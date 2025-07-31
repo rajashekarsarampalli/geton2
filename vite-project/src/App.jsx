fc0b5409fe8d72b62511477ab2ce6c92cd90072d
rajashekarsarampalli <rajashekarsarampalli@gmail.com>
2025-07-31 23:12:17 +0530

    Move ccheckout page into react

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,13 +2,14 @@
 import './App.css'
 import { Routes,Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
 function App() {
 
   return (
     <>
     <Routes>
-      <Route index path="9+/" element={<HomePage />} />
-      <Route path="checkout" element={<div>Test checkout page</div>} />
+      <Route index path="/" element={<HomePage/>} />
+      <Route path="checkout" element={<CheckoutPage/>} />
     </Routes>
    
     </>
