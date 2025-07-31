3b9596bf786bdea6443872901ec28618d1ef2e3f
rajashekarsarampalli <rajashekarsarampalli@gmail.com>
2025-07-31 22:48:15 +0530

    create multiple pages using react-router

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,16 @@
 
 import './App.css'
+import { Routes,Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 function App() {
 
   return (
     <>
-      <HomePage />
+    <Routes>
+      <Route index path="9+/" element={<HomePage />} />
+      <Route path="checkout" element={<div>Test checkout page</div>} />
+    </Routes>
+   
     </>
   )
 }
