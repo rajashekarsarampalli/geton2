4e04fc0e4e7bff97d2035d34893025f874caace3
rajashekarsarampalli <rajashekarsarampalli@gmail.com>
2025-08-04 17:49:52 +0530

    moving orders and tracking pages into app

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 import { Routes,Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
+
 function App() {
 
   return (
@@ -10,6 +13,8 @@ function App() {
     <Routes>
       <Route index path="/" element={<HomePage/>} />
       <Route path="checkout" element={<CheckoutPage/>} />
+      <Route path="orders" element={<OrdersPage></OrdersPage>}></Route>
+      <Route path="tracking" element={<TrackingPage></TrackingPage>}></Route>
     </Routes>
    
     </>
